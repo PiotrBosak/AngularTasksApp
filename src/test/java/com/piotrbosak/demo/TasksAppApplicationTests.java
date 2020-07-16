9d083efd6823a9ee2589afe72c45228ec18c1711
Piotr <PiotrBosak@yahoo.com>
2020-07-16 19:20:12 +0200

    init

---
+++ src/test/java/com/piotrbosak/demo/TasksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrbosak.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
